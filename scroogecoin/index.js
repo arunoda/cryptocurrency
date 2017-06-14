9c86aa2ee9c42ed00bc3aa9c9d4e8f2220aa1791
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-06-15 04:59:59 +0530

    Implement the proper scroogecoin.

--- scroogecoin/index.js
+++ scroogecoin/index.js
@@ -3,6 +3,7 @@ const { createUser, hashObject, signData, verify } = require('../lib/crypto')
 // This is an async user
 const scrooge = createUser()
 const store = {}
+const spentCoins = {}
 exports.lastest = null
 
 class Transaction {
@@ -14,7 +15,6 @@ class Transaction {
     this.spentCoins = null
 
     // metadata
-    this.hash = this.buildHash()
     this.signatures = {}
   }
 
@@ -38,8 +38,8 @@ exports.createCoin = async function (owner, value) {
   const { privateKey, publicKey } = await scrooge
 
   // Create coin
-  const coins = [{ id: 0, owner, value }]
-  const transaction = new Transaction(
+  const coins = [{ owner, value }]
+  const tx = new Transaction(
     exports.latest,
     'create',
     null,
@@ -47,14 +47,84 @@ exports.createCoin = async function (owner, value) {
   )
 
   // Sign it
-  const signature = signData(privateKey, transaction.hash)
-  transaction.sign(publicKey, signature)
+  const hash = tx.buildHash()
+  const signature = signData(privateKey, hash)
+  tx.sign(publicKey, signature)
 
   // Store it
-  exports.latest = transaction
-  store[transaction.hash] = transaction
+  exports.latest = tx
+  store[hash] = tx
 
-  return transaction
+  return tx
+}
+
+exports.payCoins = async function (prevCoins, newCoins, signFn) {
+  const tx = new Transaction(
+    exports.latest,
+    'pay',
+    prevCoins,
+    newCoins
+  )
+
+  let totalValue = 0
+  const coinOwners = []
+
+  // Verify prev coins
+  for (const coinId of prevCoins) {
+    // Check if this coin paid earlier
+    if (spentCoins[coinId]) {
+      throw new Error(`Coin has already paid: ${coinId}`)
+    }
+
+    // Check coin existness
+    const [hash, index] = coinId.split('::')
+    const coinTx = store[hash]
+    if (!coinTx) {
+      throw new Error('The coin has never created')
+    }
+
+    const storedCoin = coinTx.coins[parseInt(index)]
+    if (!storedCoin) {
+      throw new Error('The coin has never created')
+    }
+
+    coinOwners.push(storedCoin.owner)
+    totalValue += storedCoin.value
+  }
+
+  // Check for new coins
+  let newCoinsTotal = newCoins.reduce(
+    (total, c) => total + c.value,
+    0 
+  )
+
+  if (newCoinsTotal !== totalValue) {
+    throw new Error(`Coin totals are not the same. new: ${newCoinsTotal}, current: ${totalValue}`)
+  }
+
+  // Build the hash
+  const hash = tx.buildHash()
+  
+  // Get signatures coin owners
+  for(const owner of coinOwners) {
+    const signature = signFn(owner, hash)
+    if (!verify(owner, signature, hash)) {
+      throw new Error(`Signed signature is wrong for user: ${owner}`)
+    }
+    tx.sign(owner, signature)
+  }
+
+  // Sign the token
+  const { privateKey, publicKey } = await scrooge
+  const signature = signData(privateKey, hash)
+  tx.sign(publicKey, signature)
+
+  // Store it
+  exports.latest = tx
+  store[hash] = tx
+  prevCoins.forEach(id => spentCoins[id] = true)
+
+  return tx
 }
 
 exports.verify = async function (transaction) {
--- scroogecoin/usage.test.js
+++ scroogecoin/usage.test.js
@@ -7,7 +7,7 @@ describe('ScroogeCoin', () => {
     const tx = await ScroogeCoin.createCoin(user.publicKey, 10)
 
     expect(tx.coins).toEqual(
-      [{id: 0, owner: user.publicKey, value: 10}]
+      [{owner: user.publicKey, value: 10}]
     )
     expect(tx.type).toBe('create')
 
@@ -21,9 +21,147 @@ describe('ScroogeCoin', () => {
 
     try {
       await ScroogeCoin.verify(tx)
+      throw new Error('Should fail')
     } catch (ex) {
       if(/Invalid Transaction/.test(ex.message)) return
       throw ex
     }
   })
+
+  describe('pay coins', () => {
+    it ('should allow to pay coins', async () => {
+      const arunoda = await crypto.createUser()
+      const nadee = await crypto.createUser()
+      const jamo = await crypto.createUser()
+      const olleh = await crypto.createUser()
+
+      const arunodaCoin = await ScroogeCoin.createCoin(arunoda.publicKey, 10)
+      const nadeeCoin = await ScroogeCoin.createCoin(nadee.publicKey, 20)
+
+      const tx = await ScroogeCoin.payCoins(
+        [`${arunodaCoin.buildHash()}::0`, `${nadeeCoin.buildHash()}::0`],
+        [
+          { owner: jamo.publicKey, value: 15},
+          { owner: olleh.publicKey, value: 15}
+        ],
+        function coinOwnerSign(ownerPublicKey, hash) {
+          if (ownerPublicKey === arunoda.publicKey) {
+            return crypto.signData(arunoda.privateKey, hash)
+          }
+
+          if (ownerPublicKey === nadee.publicKey) {
+            return crypto.signData(nadee.privateKey, hash)
+          }
+
+          throw new Error('No user to sign the coin')   
+        }
+      )
+
+      expect(tx.coins).toEqual(
+        [
+          { owner: jamo.publicKey, value: 15},
+          { owner: olleh.publicKey, value: 15}
+        ]
+      )
+    })
+
+    it('should not allow to pay if totals are different', async () => {
+      const arunoda = await crypto.createUser()
+      const nadee = await crypto.createUser()
+      const jamo = await crypto.createUser()
+      const olleh = await crypto.createUser()
+
+      const arunodaCoin = await ScroogeCoin.createCoin(arunoda.publicKey, 10)
+      const nadeeCoin = await ScroogeCoin.createCoin(nadee.publicKey, 20)
+
+      try {
+        const fn = await ScroogeCoin.payCoins(
+          [`${arunodaCoin.buildHash()}::0`, `${nadeeCoin.buildHash()}::0`],
+          [
+            { owner: jamo.publicKey, value: 15},
+            { owner: olleh.publicKey, value: 20}
+          ]
+        )
+        throw new Error('Should fail')
+      } catch(ex) {
+        if (/Coin totals are not the same/.test(ex.message)) return
+        throw ex
+      }
+      
+    })
+
+    it ('should not allow to pay already spent coins', async () => {
+      const arunoda = await crypto.createUser()
+      const jamo = await crypto.createUser()
+
+      const arunodaCoin = await ScroogeCoin.createCoin(arunoda.publicKey, 10)
+
+      // Pay arunoda's coin to jamo 
+      await ScroogeCoin.payCoins(
+        [`${arunodaCoin.buildHash()}::0`],
+        [{ owner: jamo.publicKey, value: 10}],
+        (owner, hash) => crypto.signData(arunoda.privateKey, hash)
+      )
+
+      // Try to pay it again
+      try {
+        await ScroogeCoin.payCoins(
+          [`${arunodaCoin.buildHash()}::0`],
+          [{ owner: jamo.publicKey, value: 10}],
+          (owner, hash) => crypto.signData(arunoda.privateKey, hash)
+        )
+        throw new Error('Should fail')
+      } catch(ex) {
+        if(/Coin has already paid/.test(ex.message)) return
+        throw ex
+      }
+
+    })
+
+    it ('should reject coins with no transaction', async () => {
+      // Pay arunoda's coin to jamo 
+      try {
+        await ScroogeCoin.payCoins(
+          [`KKRD::0`]
+        )
+        throw new Error('Should fail')
+      } catch(ex) {
+        if(/The coin has never created/.test(ex.message)) return
+        throw ex
+      }
+    })
+
+    it ('should reject coins with no coin but a transaction', async () => {
+      const arunoda = await crypto.createUser()
+      const arunodaCoin = await ScroogeCoin.createCoin(arunoda.publicKey, 10)
+
+      // Pay arunoda's coin to jamo 
+      try {
+        await ScroogeCoin.payCoins(
+          [`${arunodaCoin.buildHash()}::10`]
+        )
+        throw new Error('Should fail')
+      } catch(ex) {
+        if(/The coin has never created/.test(ex.message)) return
+        throw ex
+      }
+    })
+
+    it ('should block if a prev transaction is modified', async () => {
+      const arunoda = await crypto.createUser()
+      const jamo = await crypto.createUser()
+
+      const arunodaCoin = await ScroogeCoin.createCoin(arunoda.publicKey, 10)
+      // arunodaCoin.coins[0].value = 20
+
+      // Pay arunoda's coin to jamo 
+      await ScroogeCoin.payCoins(
+        [`${arunodaCoin.buildHash()}::0`],
+        [{ owner: jamo.publicKey, value: 10}],
+        (owner, hash) => crypto.signData(arunoda.privateKey, hash)
+      )
+    })
+
+  })
+
 })
